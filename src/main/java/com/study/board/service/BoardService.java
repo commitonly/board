c5ee395a580697037ca09c6e9086a382fef2e756
taemin <myloopmy@gmail.com>
2022-12-23 02:01:50 +0900

    게시물 반복문 페이징 간단처리

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -14,5 +16,8 @@ public class BoardService {
     public void write(Board board){
 
         boardRepository.save(board);
+    }
+    public List<Board> boardList(){
+        return boardRepository.findAll();
     }
 }
