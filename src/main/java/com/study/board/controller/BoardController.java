20fe44a8b02705403c1b0ac9cdafd932e6b6b9dc
taemin <myloopmy@gmail.com>
2022-12-23 01:55:21 +0900

    게시물 페이징 리스트작업

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,18 +11,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm(){
 
         return "boardwrite";
     }
     @PostMapping("/board/writepro")
-    public String boardWritePro(String title, String content){
+    public String boardWritePro(Board board){
 
-        System.out.println("제목 : " + title);
-        System.out.println("내용 : " + content);
+        boardService.write(board);
 
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(){
+        return "boardlist";
     }
 
 }
