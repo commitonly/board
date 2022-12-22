e2fd739709d1ffdd6ac02e8cc3a3433ca4a70986
taemin <myloopmy@gmail.com>
2022-12-23 03:20:15 +0900

    게시물 상세페이지 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -34,4 +34,10 @@ public class BoardController {
         return "boardlist";
     }
 
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardview";
+    }
 }
