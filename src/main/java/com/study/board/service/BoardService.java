280c98cda2d6809c94b0eb94f56e84ff8389c5d5
taemin <myloopmy@gmail.com>
2022-12-30 19:40:43 +0900

    페이징처리

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,20 +40,26 @@ public class BoardService {
     }
 
     // 게시글 리스트 처리
-    public List<Board> boardList(){
-        return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable)
+    {
+        return boardRepository.findAll(pageable);
+
     }
 
     // 특정 게시글 불러오기
 
-    public Board boardview(Integer id){
+    public Board boardview(Integer id)
+    {
         return boardRepository.findById(id).get();
+
     }
 
     // 특정 게시글 삭제
 
-    public void boardDelete(Integer id){
+    public void boardDelete(Integer id)
+    {
         boardRepository.deleteById(id);
+
     }
 
 }
