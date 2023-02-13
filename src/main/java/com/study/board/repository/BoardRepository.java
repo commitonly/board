201b6bf71391e8dbf00e5b3c79df45b4e911fbab
taemin <myloopmy@gmail.com>
2023-02-14 00:02:56 +0900

    // Marking : 토이프로젝트 기간 2주 처리중으로 잠정보류 <- 해당 프로젝트 종료 후 실습 부족 시 해당 레포 재실행 예정. fin

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -9,3 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
 }
+
+
+// Marking : 토이프로젝트 기간 2주 처리중으로 잠정보류 <- 해당 프로젝트 종료 후 실습 부족 시 해당 레포 재실행 예정. fin
